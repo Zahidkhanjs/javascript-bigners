12a3d732fcea3e78a7f66746afc55bbdb4a5078f
zahidkhanjs <zahidshahwani9925@gmail.com>
2025-11-23 22:36:07 +0500

    operators practice compleated

---
+++ operators/operotors/app.js
@@ -0,0 +1,119 @@
+// Operators
+// there are 6 Operators
+
+// 1 arithematic operators
+// 2 comparision operators
+// 3 assignment operators
+// 4 logical operators
+// 5 unary operators
+// 6 Binary operators
+
+// let x = 10
+// let y = 20
+
+// if (x > 5 && y < 25  ){
+//     console.log("A");
+// }else{
+//     console.log("B");
+// }
+
+
+
+// let login = "true"
+// let signin = "false"
+
+// if ( login || signin ){
+//     console.log("welcome to me ");
+
+// }else{
+//     console.log("sorry byby");
+// }
+
+
+// let temperature = 40
+
+// if (!(temperature < 30)){
+//     console.log("hot");
+// }else{
+//     console.log("cold");
+// }
+
+
+// let a = 0
+
+// if (a) {
+//     console.log("truthy");
+
+// }else{
+//     console.log("falsy");
+// }
+
+
+
+
+// let score = 60
+
+// let head = score >= 90 ? "a" : score >= 70 ? "b" : score >= 56 ? "c"
+// : "false"
+// console.log(head);
+
+
+
+// let pointer = 120
+// let code = pointer > 100 ? "good " : pointers > 50 ? "bad"
+// : "by"
+// console.log(code);
+
+// let loggend = true
+// let token = false 
+
+// let home = loggend && token ? "allow " : " ok by"
+// console.log(home);
+
+// let b = 30
+// ++b
+// console.log(b);
+
+// let a = 30
+// a++ 
+// console.log(a);
+
+//  let c = 20
+//  c--
+//  console.log(c);
+
+// let c = 20
+// --c
+// console.log(c);
+
+
+// let x = 3
+// let y = x++
+// console.log(x , y);
+
+
+// let a = 10
+// console.log(a--);
+// console.log(a);
+
+
+
+// let  b = 5
+// let rezult = ++b + b++
+// console.log(rezult);
+
+// let likes = 100
+// function likepost(){
+//     return ++likes 
+// }
+// console.log(likepost());
+// console.log(likes);
+
+// let count = 5
+
+// if (count-- === 5){
+//     console.log('mached');
+
+// }else{
+//     console.log('nomached');
+// }
