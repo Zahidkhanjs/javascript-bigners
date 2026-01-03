b51435aee3ebc595343800e897e20747b937cd54
zahidkhanjs <zahidshahwani9925@gmail.com>
2026-01-03 21:47:34 +0500

    para edit

---
+++ todo app/app.js
@@ -0,0 +1,36 @@
+
+const input = document.querySelector("#input");
+const ol = document.querySelector("#list");
+
+let allTodos = [];
+
+function renderTodo() {
+  ol.innerHTML = "";
+  for (let i = 0; i < allTodos.length; i++) {
+    ol.innerHTML += ` <li>
+                <span class="todo-text">${allTodos[i]}</span>
+                <div class="actions">
+                    <button class="edit-btn" onclick="editTodo(${i})">Edit</button>
+                    <button class="delete-btn" onclick="deleteTodo(${i})">Delete</button>
+                </div>
+            </li>`;
+  }
+}
+
+function addTodo() {
+  allTodos.push(input.value);
+  renderTodo()
+}
+
+function deleteTodo(index) {
+  console.log("todo deleted ", index);
+  allTodos.splice(index, 1);
+  console.log(allTodos);
+
+ renderTodo()
+}
+
+
+function editTodo(index) {
+  console.log("todo edited ", index);
+}
